c1770074781d51075baa5dd6de2ed27dd00205c7
ShaohonChen <shaohon_chen@115lab.club>
2025-02-19 02:33:10 +0800

    add 3bi exp

--- scripts/download_model_and_datasets.sh
+++ scripts/download_model_and_datasets.sh
@@ -3,5 +3,5 @@
 
 # 下载模型
 # modelscope download --model Qwen/Qwen2.5-0.5B --local_dir /mnt/work/weights/Qwen2__5-0__5B
-modelscope download --model Qwen/Qwen2.5-0.5B-Instruct --local_dir /mnt/work/weights/Qwen2__5-0__5B-Instruct
-# modelscope download --model Qwen/Qwen2.5-3B --local_dir /mnt/work/weights/Qwen2__5-3B
+# modelscope download --model Qwen/Qwen2.5-0.5B-Instruct --local_dir /mnt/work/weights/Qwen2__5-0__5B-Instruct
+modelscope download --model Qwen/Qwen2.5-3B-Instruct --local_dir /mnt/work/weights/Qwen2__5-3B-Instruct
