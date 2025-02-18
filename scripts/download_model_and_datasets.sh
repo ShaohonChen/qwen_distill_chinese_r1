cb3991753fe37207e87aa1efda5bf55bea7eaa9c
ShaohonChen <shaohon_chen@115lab.club>
2025-02-18 23:40:40 +0800

    add 05BI exp

--- scripts/download_model_and_datasets.sh
+++ scripts/download_model_and_datasets.sh
@@ -1,6 +1,7 @@
 # 下载数据集
-modelscope download --dataset liucong/Chinese-DeepSeek-R1-Distill-data-110k  --local_dir /mnt/work/datasets/Chinese-DeepSeek-R1-Distill-data-110k
+# modelscope download --dataset liucong/Chinese-DeepSeek-R1-Distill-data-110k  --local_dir /mnt/work/datasets/Chinese-DeepSeek-R1-Distill-data-110k
 
 # 下载模型
-modelscope download --model Qwen/Qwen2.5-0.5B --local_dir /mnt/work/weights/Qwen2__5-0__5B
-modelscope download --model Qwen/Qwen2.5-3B --local_dir /mnt/work/weights/Qwen2__5-3B
+# modelscope download --model Qwen/Qwen2.5-0.5B --local_dir /mnt/work/weights/Qwen2__5-0__5B
+modelscope download --model Qwen/Qwen2.5-0.5B-Instruct --local_dir /mnt/work/weights/Qwen2__5-0__5B-Instruct
+# modelscope download --model Qwen/Qwen2.5-3B --local_dir /mnt/work/weights/Qwen2__5-3B
