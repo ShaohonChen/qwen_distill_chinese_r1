60e2b1c63e3c92d896fc245f6a80f24c56bbb47b
ShaohonChen <shaohon_chen@115lab.club>
2025-02-19 02:34:04 +0800

    downlaod 7b model

--- scripts/download_model_and_datasets.sh
+++ scripts/download_model_and_datasets.sh
@@ -4,4 +4,6 @@
 # 下载模型
 # modelscope download --model Qwen/Qwen2.5-0.5B --local_dir /mnt/work/weights/Qwen2__5-0__5B
 # modelscope download --model Qwen/Qwen2.5-0.5B-Instruct --local_dir /mnt/work/weights/Qwen2__5-0__5B-Instruct
-modelscope download --model Qwen/Qwen2.5-3B-Instruct --local_dir /mnt/work/weights/Qwen2__5-3B-Instruct
+# modelscope download --model Qwen/Qwen2.5-3B-Instruct --local_dir /mnt/work/weights/Qwen2__5-3B-Instruct
+modelscope download --model Qwen/Qwen2.5-7B-Instruct --local_dir /mnt/work/weights/Qwen2__5-7B-Instruct
+modelscope download --model Qwen/Qwen2.5-7B --local_dir /mnt/work/weights/Qwen2__5-7B
